e09bf6dcdb802d2a9524f4277e7e7fcab416aafb
Zai Fleitas <39562291+zai0811@users.noreply.github.com>
2024-08-25 22:36:03 -0400

    vista login y registro

--- proyecto-mern-cliente/proyecto-mern/src/App.js
+++ proyecto-mern-cliente/proyecto-mern/src/App.js
@@ -7,12 +7,16 @@ import PerfilMascota from './views/PerfilMascota';
 import HomeAdoptante from './views/HomeAdoptante';
 import './App.css';
 import HomeRescatista from './views/HomeRescatista';
+import Registro from './components/FormularioRegistro';
+import Login  from './components/FormularioLogin';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+        <Route path="/" element={<Registro />} />
+
           <Route path="/PerfilRescatista" element={<PerfilRescatista />} />
           <Route path="/pets/new" element={<AgregarMascota />} />
           <Route path="/pets/:id/edit" element={<EditarMascota />} />
